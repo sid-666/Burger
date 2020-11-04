2b51239f6186ac9847d7b0574c641d004cee8109
Seance <siddharth.dhananjay@gmail.com>
2020-11-04 14:53:28 +1100

    Did the insertOne method for the burgers model

--- models/cat.js
+++ models/cat.js
@@ -1,15 +1,15 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var cat = {
+var burgers = {
   selectAll: function(cb) {
     orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
