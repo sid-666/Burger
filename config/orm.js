05981ca159f3714d5bf2368ffdcdc4983589fe3d
Seance <siddharth.dhananjay@gmail.com>
2020-11-04 14:48:31 +1100

    did the updateOne method in the ORM

--- config/orm.js
+++ config/orm.js
@@ -71,7 +71,7 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
+  updateOne: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
