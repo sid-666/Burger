9745910ef9038c33a3ac662435d7ff9dd219ab6e
Seance <siddharth.dhananjay@gmail.com>
2020-11-04 21:20:48 +1100

    populated it with data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
+INSERT INTO burgers (burger_name) VALUES ('Wopper');
+INSERT INTO burgers (burger_name) VALUES ('Macncheese');
+INSERT INTO burgers (burger_name) VALUES ('Kentucky burger');
+
