53de909ca5c07dd1f3fe936face808a539353e8d
Seance <siddharth.dhananjay@gmail.com>
2020-11-04 15:16:07 +1100

    Had to update route path due to change in file names

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
 });
